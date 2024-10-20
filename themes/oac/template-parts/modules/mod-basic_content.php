6585299687f9cc109939de24a8c2dd5f34f9d9d1
alexpstein <alexstein47@gmail.com>
2024-10-20 16:43:56 -0400

    Add basic content module

---
+++ themes/oac/template-parts/modules/mod-basic_content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Basic Content
+ */
+?>
+
+<div class="basic-content module">
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-12">
+                <?php echo get_sub_field('content'); ?>
+            </div>
+        </div>
+    </div>
+</div>
