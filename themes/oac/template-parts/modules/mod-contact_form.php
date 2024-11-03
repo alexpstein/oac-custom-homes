0562769ea379cc7fee0bdb54521d65d75a4422cf
alexpstein <alexstein47@gmail.com>
2024-11-03 14:22:26 -0500

    Configured contact form module to have global option

--- themes/oac/template-parts/modules/mod-contact_form.php
+++ themes/oac/template-parts/modules/mod-contact_form.php
@@ -9,8 +9,17 @@
         <div class="row">
             <div class="col-12">
                 <?php
-                if ( ! empty( get_sub_field('heading') ) ) echo '<h2 class="contact-form__heading">' . get_sub_field('heading') . '</h2>';
-                if ( ! empty( get_sub_field('body_text') ) ) echo wpautop( get_sub_field('body_text') );
+                $use_global = get_sub_field('use_global_contact_form_text');
+                $contact_heading = get_field('global_contact_form_header', 'option');
+                $contact_body = get_field('global_contact_form_text', 'option');
+
+                if ( $use_global == false ) {
+                    $contact_heading = get_sub_field('heading');
+                    $contact_body = get_sub_field('body_text');
+                }
+                
+                echo '<h2 class="contact-form__heading">' . $contact_heading . '</h2>';
+                echo wpautop( $contact_body );
                 ?>
                 <?php echo do_shortcode( '[gravityform id="' . get_sub_field('form_id') . '" title="false" description="true"]'); ?>
                 <script>
