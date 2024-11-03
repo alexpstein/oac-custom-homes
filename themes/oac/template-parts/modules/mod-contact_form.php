8ff35c69add67857dcb4dc227ab1b266e7c7e44f
alexpstein <alexstein47@gmail.com>
2024-11-03 15:44:17 -0500

    Add formatting options to contact form module

--- themes/oac/template-parts/modules/mod-contact_form.php
+++ themes/oac/template-parts/modules/mod-contact_form.php
@@ -2,22 +2,29 @@
 /**
  * Contact Form
  */
+
+$use_global = get_sub_field('use_global_contact_form_text');
+$contact_heading = get_field('global_contact_form_header', 'option');
+$contact_body = get_field('global_contact_form_text', 'option');
+$use_divider = get_sub_field('use_divider');
+$alt_divider = get_sub_field('use_alt_divider_style');
+$divider_class = 'hr-default';
+$reduce_top_padding = get_sub_field('reduce_top_padding');
+
+if ( $use_global == false ) {
+    $contact_heading = get_sub_field('heading');
+    $contact_body = get_sub_field('body_text');
+}
+
+if ( $alt_divider ) $divider_class = 'hr-alt';
 ?>
 
-<div class="contact-form module">
+<div class="contact-form module <?php if ( $reduce_top_padding ) echo 'contact-form--pt0'; ?>">
     <div class="container-lg">
         <div class="row">
             <div class="col-12">
                 <?php
-                $use_global = get_sub_field('use_global_contact_form_text');
-                $contact_heading = get_field('global_contact_form_header', 'option');
-                $contact_body = get_field('global_contact_form_text', 'option');
-
-                if ( $use_global == false ) {
-                    $contact_heading = get_sub_field('heading');
-                    $contact_body = get_sub_field('body_text');
-                }
-                
+                if ( $use_divider ) echo '<hr class="' . $divider_class . '">';
                 echo '<h2 class="contact-form__heading">' . $contact_heading . '</h2>';
                 echo wpautop( $contact_body );
                 ?>
