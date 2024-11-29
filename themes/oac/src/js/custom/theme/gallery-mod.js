c019f10792d04edf5a647bcd25a794687deaa374
alexpstein <alexstein47@gmail.com>
2024-11-29 17:08:49 -0500

    Add Gallery (Group) module

--- themes/oac/src/js/custom/theme/gallery-mod.js
+++ themes/oac/src/js/custom/theme/gallery-mod.js
@@ -15,7 +15,7 @@ if ( jQuery('.gallery__slick').length ) {
 }
 
 /**
- * Expandable gallery functionality
+ * Gallery elements
  */
 const expandBtns = document.querySelectorAll('.gallery__btn');
 const closeBtn = document.querySelector('.gallery__close');
@@ -85,3 +85,32 @@ jQuery('button[data-slide]').click( function(e) {
     let slideNo = jQuery(this).data('slide');
     jQuery('.gallery__slick').slick('slickGoTo', slideNo - 1);
 });
+
+/**
+ * Functions to enable focus on buttons for modal galleries
+ * and refresh for proper display
+ */
+
+function galleryModalOpen() {
+    // Refresh carousel when modal opened to display properly
+    jQuery('.gallery__slick').slick('refresh');
+
+    jQuery('.gallery__slick').slick('slickGoTo', 0);
+
+    jQuery('.gallery__slick').slick('slickSetOption', {
+        prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous Image" role="button" style="display: inline-block;"><div class="arrow"><span></span><span></span></div></button>',
+        nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next Image" role="button" style="display: inline-block;"><div class="arrow"><span></span><span></span></div></button>'
+    }, true);
+}
+
+/**
+ * Add event listeners to gallery modals if they exist on the page
+ */
+
+const galleryModals = document.querySelectorAll('.gallery__dialog');
+
+if ( document.querySelector( '.gallery__dialog' ) ) {
+    galleryModals.forEach(modal => {
+        modal.addEventListener( 'shown.bs.modal', galleryModalOpen );
+    });
+}
---
+++ themes/oac/template-parts/modules/mod-gallery_group.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Gallery Group
+ */
+?>
+
+<div class="gallery-group module">
+    <div class="container-lg">
+        <?php
+        if ( ! empty( get_sub_field('heading') ) ) echo '<h2 class="gallery-group__heading">' . get_sub_field('heading') . '</h2>';
+        if ( ! empty( get_sub_field('body_text') ) ) echo wpautop( get_sub_field('body_text') );
+        if ( have_rows('galleries') ) :
+            $i = 0;
+        ?>
+            <ul class="gallery-group__flex">
+            <?php
+            while ( have_rows('galleries') ) : the_row();
+                $gallery_name = get_sub_field('gallery_title');
+                $images = get_sub_field('gallery');
+                $i++;
+            ?>
+                <li class="gallery-group__li">
+                    <button type="button" class="gallery-group__btn" data-bs-toggle="modal" data-bs-target="#gallery-modal-<?php echo $i; ?>"  aria-label="<?php echo sprintf( __( 'View gallery for %s', '_themename' ), $gallery_name ); ?>">
+                        <div class="gallery-group__btn-label">
+                            <span class="gallery-group__btn-label-text"><?php echo get_sub_field('gallery_title'); ?></span>
+                        </div>
+                        <img src="<?php echo esc_url( $images[0]['sizes']['thumbnail'] ); ?>" class="gallery__thumb" alt="">
+                    </button>
+                </li>
+            <?php
+            endwhile;
+            ?>
+            </ul>
+        <?php
+        endif;
+        if ( have_rows('galleries') ) :
+            reset_rows();
+            $i = 0;
+            while ( have_rows('galleries') ) : the_row();
+                $images = get_sub_field('gallery');
+                $i++;
+            ?>
+            <div class="modal fade gallery__dialog" id="gallery-modal-<?php echo $i; ?>" aria-label="<?php echo sprintf( __('%s gallery', '_themename'), get_sub_field('gallery_title') ); ?>" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-full-width modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?php _e( 'Close', '_themename' ); ?>">&times;</button>
+                        </div>
+                        <div class="modal-body gallery__modal">
+                            <div class="gallery__slick">
+                                <?php
+                                foreach( $images as $img ):
+                                ?>
+                                <div>
+                                    <div class="gallery__el">
+                                        <img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo $img['alt']; ?>" class="gallery__img">
+                                    </div>
+                                </div>
+                                <?php
+                                endforeach;
+                                ?>
+                            </div>
+                            <div class="gallery__modal-btns">
+                                <?php
+                                $slide = 0;
+                                foreach( $images as $img ):
+                                    $slide++;
+                                ?>
+                                <button class="gallery__modal-btn" data-slide="<?php echo $slide; ?>" aria-label="<?php echo sprintf( __( 'Go to slide %s', '_themename' ), $slide ); ?>">
+                                    <img src="<?php echo esc_url( $img['sizes']['thumbnail'] ); ?>" alt="" class="gallery__btn-img">
+                                </button>
+                                <?php
+                                endforeach;
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php
+            endwhile;
+        endif;
+        ?>
+    </div>
+</div>
